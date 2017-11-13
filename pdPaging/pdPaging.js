00c9c54ce287d3c9cf7338710e4cf0ea32b72092
liuhefa <faheliu@163.com>
2017-11-13 17:45:37 +0800

    paging-dev

--- pdPaging/pdPaging.js
+++ pdPaging/pdPaging.js
@@ -8,7 +8,7 @@
             nextText: '&raquo;',
             container: '#pagination',
             callback: function (num) {
-                console.log(num+'callback')
+                console.log(num + 'callback')
                 return false
             }
         }
@@ -24,7 +24,7 @@
         render: function () {
             let _this = this
             let pageAmount = Math.ceil(_this.options.totalSize / _this.options.pageSize)
-            if(_this.options.pageNo>pageAmount){
+            if (_this.options.pageNo > pageAmount) {
                 return
             }
 
@@ -36,7 +36,7 @@
             let goEle = stringToDom('<li>去<input type="text" id="go-page-num">页<button>跳转</button></li>')
 
 
-            let currentNo =parseInt(_this.options.pageNo)
+            let currentNo = parseInt(_this.options.pageNo)
             let startNo = currentNo - 2
             let endNo = currentNo + 2
 
@@ -59,26 +59,26 @@
                     endNo = pageAmount
                 }
 
-                if(startNo===2){
-                    startNo=1
+                if (startNo === 2) {
+                    startNo = 1
                 }
-                if(endNo===(pageAmount-1)){
-                    endNo=pageAmount
+                if (endNo === (pageAmount - 1)) {
+                    endNo = pageAmount
                 }
             }
             //ulEle.appendChild(prevEle)
             addPrev()
-            if(startNo===1){
+            if (startNo === 1) {
                 addPageNum(startNo, endNo, currentNo)
-            }else{
-                let firstEle=stringToDom('<li ><span class="page-span" data-pn="1">1</span></li>')
+            } else {
+                let firstEle = stringToDom('<li ><span class="page-span" data-pn="1">1</span></li>')
                 ulEle.appendChild(firstEle)
                 addDot()
                 addPageNum(startNo, endNo, currentNo)
             }
-            if(endNo<pageAmount){
+            if (endNo < pageAmount) {
                 addDot()
-                let lastEle=stringToDom('<li ><span class="page-span" data-pn="' + pageAmount + '">' + pageAmount + '</span></li>')
+                let lastEle = stringToDom('<li ><span class="page-span" data-pn="' + pageAmount + '">' + pageAmount + '</span></li>')
                 ulEle.appendChild(lastEle)
             }
 
@@ -86,14 +86,15 @@
             //ulEle.appendChild(nextEle)
             addNext()
             ulEle.appendChild(goEle)
-            containerEle.innerHTML=''
+            containerEle.innerHTML = ''
             containerEle.appendChild(ulEle)
             addTotalNum()
+
             function addPageNum(start, end, current) {
                 console.log(start, end, current)
                 for (let i = start; i <= end; i++) {
                     let pageEle;
-                    if (i ===parseInt(current)) {
+                    if (i === parseInt(current)) {
                         console.log(current)
                         pageEle = stringToDom('<li><span  class="page-span active-page" data-pn="' + i + '">' + i + '</span></li>')
                     } else {
@@ -105,18 +106,32 @@
             }
 
             function addPrev() {
-                let prevEle = stringToDom('<li><span class="prev-span">' + _this.options.prevText + '</span></li>')
+                let prevEle;
+                if (currentNo === 1) {
+                    prevEle = stringToDom('<li><span class="prev-span paging-disabled">' + _this.options.prevText + '</span></li>')
+                } else {
+                    prevEle = stringToDom('<li><span class="prev-span">' + _this.options.prevText + '</span></li>')
+                }
+
                 ulEle.appendChild(prevEle)
             }
 
             function addNext() {
-                let nextEle = stringToDom('<li><span class="next-span">' + _this.options.nextText + '</span></li>')
+                let nextEle
+                if (currentNo === endNo) {
+                    nextEle = stringToDom('<li><span class="next-span paging-disabled">' + _this.options.nextText + '</span></li>')
+                } else {
+                    nextEle = stringToDom('<li><span class="next-span">' + _this.options.nextText + '</span></li>')
+                }
+
                 ulEle.appendChild(nextEle)
             }
+
             function addDot() {
                 let dotEle = stringToDom('<li><span>...</span></li>')
                 ulEle.appendChild(dotEle)
             }
+
             function addTotalNum() {
                 let totalEle = stringToDom('<li>共' + _this.options.totalSize + '条</li>')
                 ulEle.appendChild(totalEle)
@@ -125,35 +140,39 @@
             function pageEvent(e) {
                 let target = e.target || e.srcElement
                 let matchesSelector = target.matches || target.webkitMatchesSelector || target.mozMatchesSelector || target.msMatchesSelector;
-                if(matchesSelector.call(target,'.page-span')){
-                    let num=target.getAttribute('data-pn')
+                if (matchesSelector.call(target, '.page-span')) {
+                    let num = target.getAttribute('data-pn')
                     _this.options.callback(num)
                 }
                 //上一页
-                if(matchesSelector.call(target,'.prev-span')){
-                    console.log(_this.options.pageNo)
-                    if(_this.options.pageNo<=1){
+                if (matchesSelector.call(target, '.prev-span')) {
+                    if (target.className.indexOf('paging-disabled') > -1) {
+                        return false
+                    }
+                    if (_this.options.pageNo <= 1) {
                         _this.options.callback(1)
-                    }else{
-                        _this.options.callback(_this.options.pageNo-1)
+                    } else {
+                        _this.options.callback(_this.options.pageNo - 1)
                     }
                 }
                 //下一页
-                if(matchesSelector.call(target,'.next-span')){
-                    console.log(_this.options.pageNo)
-                    if(_this.options.pageNo>=pageAmount){
+                if (matchesSelector.call(target, '.next-span')) {
+                    if (target.className.indexOf('paging-disabled') > -1) {
+                        return false
+                    }
+                    if (_this.options.pageNo >= pageAmount) {
                         _this.options.callback(pageAmount)
-                    }else{
-                        _this.options.callback(_this.options.pageNo+1)
+                    } else {
+                        _this.options.callback(_this.options.pageNo + 1)
                     }
                 }
 
-                if(target.nodeName.toLowerCase()==='button'){
-                    let inputPageNo=document.getElementById('go-page-num').value;
-                    let numReg=/^\d+$/
-                    if(numReg.test(inputPageNo)){
+                if (target.nodeName.toLowerCase() === 'button') {
+                    let inputPageNo = document.getElementById('go-page-num').value;
+                    let numReg = /^\d+$/
+                    if (numReg.test(inputPageNo)) {
                         _this.options.callback(inputPageNo)
-                    }else{
+                    } else {
                         return false
                     }
                 }
@@ -162,7 +181,7 @@
             }
 
             //事件绑定
-            ulEle.addEventListener("click",pageEvent,false)
+            ulEle.addEventListener("click", pageEvent, false)
 
         }
     };
@@ -174,14 +193,14 @@
     }
 
     //兼容模块
-    if(typeof module !=='undefined' && typeof exports ==='object'){
-        module.exports=paging;
-    }else if(typeof define ==='function' && (define.amd || define.cmd)){
-        define("pdPaging",function(){
+    if (typeof module !== 'undefined' && typeof exports === 'object') {
+        module.exports = paging;
+    } else if (typeof define === 'function' && (define.amd || define.cmd)) {
+        define("pdPaging", function () {
             return paging;
         })
-    }else{
-        window.pdPaging=paging;
+    } else {
+        window.pdPaging = paging;
     }
 
 
